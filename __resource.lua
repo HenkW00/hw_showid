76125c268e0886e52af551fd40a8bf72dd869b3c
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-04-22 11:49:53 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 author 'HenkW'
 description 'Simple ID Menu using jsfour ID card'
-version '1.0.2'
+version '1.0.3'
 
 client_scripts {
 	'client.lua',
